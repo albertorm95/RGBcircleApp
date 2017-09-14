688a3a538c02aa72df8b030f05783b1374aaf201
econfig <econfig@econfigs-macbook-air-5.local>
2017-09-13 22:34:58 -0500

    Initial Commit

---
+++ CircleAndSliders/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CircleAndSliders
+//
+//  Created by econfig on 9/13/17.
+//  Copyright © 2017 amrojas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
